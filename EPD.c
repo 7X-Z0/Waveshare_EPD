750bff4c18f58597b9214f4ba7e54fec2ebc2865
Lorenzo G. <hifi@moonjelly>
2026-08-22 09:37:41 -0400

    added bitmap implementation

--- EPD.c
+++ EPD.c
@@ -11,3 +11,35 @@ void bd_set_pixel (int x, int y, bool state){
         BD_FRAMEBUFFER[pos] &= ~(0x80 >> (x & 0x7));
     }
 }
+
+bool bd_get_pixel (int x, int y){
+    int pos = (BD_FB_WIDTH * y + x) >> 3; //bitshift 3 is the same as divided by 8
+
+    return BD_FRAMEBUFFER[pos] & (0x80 >> (x & 0x7));
+}
+
+void bd_draw_bitmap(int x, int y, int width, int height, const uint8_t *bitmap){
+    uint8_t chkMsk = 0;
+    int size = width * height;
+
+    int originalX = x;
+    int originalY = y;
+
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < 8; j++){
+            chkMsk = 0x80 >> j;
+
+            bd_set_pixel(x, y, bitmap[i] & chkMsk);
+            x++;
+
+            if ((x - originalX) == width){
+                x = originalX;
+                y++;
+            }
+
+            if ((y - originalY) == height){
+                return;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-If there's no compatibility, make it yourself
+I can't find anyone whos made a driver that's compatible with the Waveshare 2.13 inch V4 display sepcifically for the ESP32 with ESP-IDF   
+
+I might as well make it myself  
+
+
+"If there's no compatibility, make it yourself" -Unknown
